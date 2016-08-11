9e9f3fca85e8c82a199becd73bfccdbedbf8c4ab
Mathieu <m.delp@live.com>
2016-08-11 18:10:40 -0400

    160811-01_Design

--- PokeManager/PokeManager/Form1.Designer.cs
+++ PokeManager/PokeManager/Form1.Designer.cs
@@ -28,60 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.StartupSettings = new System.Windows.Forms.TabPage();
+            this.HumanizeSettings = new System.Windows.Forms.TabPage();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pokeManagerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.InventorySettings = new System.Windows.Forms.TabPage();
+            this.PokemonSettings = new System.Windows.Forms.TabPage();
+            this.SniperSettings = new System.Windows.Forms.TabPage();
+            this.LocationFeeder = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Controls.Add(this.StartupSettings);
+            this.tabControl1.Controls.Add(this.HumanizeSettings);
+            this.tabControl1.Controls.Add(this.InventorySettings);
+            this.tabControl1.Controls.Add(this.PokemonSettings);
+            this.tabControl1.Controls.Add(this.SniperSettings);
+            this.tabControl1.Controls.Add(this.LocationFeeder);
+            this.tabControl1.Font = new System.Drawing.Font("SansSerif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.tabControl1.ItemSize = new System.Drawing.Size(400, 50);
+            this.tabControl1.Location = new System.Drawing.Point(12, 31);
             this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(25, 3);
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1023, 569);
+            this.tabControl1.Size = new System.Drawing.Size(908, 604);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // StartupSettings
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1015, 543);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.StartupSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.StartupSettings.ForeColor = System.Drawing.Color.White;
+            this.StartupSettings.Location = new System.Drawing.Point(4, 54);
+            this.StartupSettings.Margin = new System.Windows.Forms.Padding(0);
+            this.StartupSettings.Name = "StartupSettings";
+            this.StartupSettings.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.StartupSettings.Size = new System.Drawing.Size(900, 546);
+            this.StartupSettings.TabIndex = 0;
+            this.StartupSettings.Text = "Startup Settings";
+            this.StartupSettings.Click += new System.EventHandler(this.StartupSettings_Click);
             // 
-            // tabPage2
+            // HumanizeSettings
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.HumanizeSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.HumanizeSettings.ForeColor = System.Drawing.Color.White;
+            this.HumanizeSettings.Location = new System.Drawing.Point(4, 54);
+            this.HumanizeSettings.Name = "HumanizeSettings";
+            this.HumanizeSettings.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.HumanizeSettings.Size = new System.Drawing.Size(1156, 546);
+            this.HumanizeSettings.TabIndex = 1;
+            this.HumanizeSettings.Text = "Humanize Settings";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip2.Text = "PokeManager";
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.BackColor = System.Drawing.Color.Brown;
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip3.Text = "PokeManager";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Brown;
+            this.menuStrip1.Font = new System.Drawing.Font("SansSerif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pokeManagerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(1, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(930, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "TitleMenu";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // pokeManagerToolStripMenuItem
+            // 
+            this.pokeManagerToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.pokeManagerToolStripMenuItem.Margin = new System.Windows.Forms.Padding(0, 2, 0, 2);
+            this.pokeManagerToolStripMenuItem.Name = "pokeManagerToolStripMenuItem";
+            this.pokeManagerToolStripMenuItem.Size = new System.Drawing.Size(109, 20);
+            this.pokeManagerToolStripMenuItem.Text = "PokeManager";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Brown;
+            this.statusStrip1.Location = new System.Drawing.Point(1, 638);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(930, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Brown;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1, 660);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Brown;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(931, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1, 660);
+            this.panel2.TabIndex = 7;
+            // 
+            // InventorySettings
+            // 
+            this.InventorySettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.InventorySettings.ForeColor = System.Drawing.Color.White;
+            this.InventorySettings.Location = new System.Drawing.Point(4, 54);
+            this.InventorySettings.Name = "InventorySettings";
+            this.InventorySettings.Size = new System.Drawing.Size(1156, 546);
+            this.InventorySettings.TabIndex = 2;
+            this.InventorySettings.Text = "Inventory Settings";
+            // 
+            // PokemonSettings
+            // 
+            this.PokemonSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.PokemonSettings.ForeColor = System.Drawing.Color.White;
+            this.PokemonSettings.Location = new System.Drawing.Point(4, 54);
+            this.PokemonSettings.Name = "PokemonSettings";
+            this.PokemonSettings.Size = new System.Drawing.Size(1156, 546);
+            this.PokemonSettings.TabIndex = 3;
+            this.PokemonSettings.Text = "Pokemon Settings";
+            // 
+            // SniperSettings
+            // 
+            this.SniperSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.SniperSettings.ForeColor = System.Drawing.Color.White;
+            this.SniperSettings.Location = new System.Drawing.Point(4, 54);
+            this.SniperSettings.Name = "SniperSettings";
+            this.SniperSettings.Size = new System.Drawing.Size(1156, 546);
+            this.SniperSettings.TabIndex = 4;
+            this.SniperSettings.Text = "Sniper Settings";
+            // 
+            // LocationFeeder
+            // 
+            this.LocationFeeder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.LocationFeeder.ForeColor = System.Drawing.Color.White;
+            this.LocationFeeder.Location = new System.Drawing.Point(4, 54);
+            this.LocationFeeder.Name = "LocationFeeder";
+            this.LocationFeeder.Size = new System.Drawing.Size(900, 546);
+            this.LocationFeeder.TabIndex = 5;
+            this.LocationFeeder.Text = "Location Feeder";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1047, 593);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(932, 660);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("SansSerif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage StartupSettings;
+        private System.Windows.Forms.TabPage HumanizeSettings;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TabPage InventorySettings;
+        private System.Windows.Forms.TabPage PokemonSettings;
+        private System.Windows.Forms.TabPage SniperSettings;
+        private System.Windows.Forms.TabPage LocationFeeder;
+        private System.Windows.Forms.ToolStripMenuItem pokeManagerToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- PokeManager/PokeManager/Form1.cs
+++ PokeManager/PokeManager/Form1.cs
@@ -16,5 +16,15 @@ namespace PokeManager
         {
             InitializeComponent();
         }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            StartupSettings.ForeColor = Color.BlanchedAlmond;
+        }
+
+        private void StartupSettings_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
