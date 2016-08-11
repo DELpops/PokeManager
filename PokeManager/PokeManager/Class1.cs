94df6475707957eafb364d83d87b279f8e32c414
Nicolai Frisvold <nicco2use@gmail.com>
2016-08-11 23:15:41 +0200

    Made a custom messagebox

--- PokeManager/PokeManager/Class1.cs
+++ PokeManager/PokeManager/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,23 @@ namespace PokeManager
 
         public void save_settings(string save_name, string[] settings)
         {
-
+            if (save_name_exists(save_name))
+            {
+               
+            }
         }
+
+        public bool save_name_exists(string save_name)
+        {
+            if (File.Exists(save_name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
---
+++ PokeManager/PokeManager/MessageBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokeManager
+{
+    public partial class MessageBox:System.Windows.Forms.Form
+    {
+        public MessageBox(string message, string buttonText1, string buttonText2)
+        {
+            label1.Text = message;
+            button1.Text = buttonText1;
+            button2.Text = buttonText2;
+        }
+    }
+}
