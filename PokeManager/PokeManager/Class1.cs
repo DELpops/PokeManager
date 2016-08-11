27d1505d9baf7c4cfd96602fe5af40534aea1125
Nicolai Frisvold <nicco2use@gmail.com>
2016-08-11 22:53:57 +0200

    Added function save_settings

--- PokeManager/PokeManager/Class1.cs
+++ PokeManager/PokeManager/Class1.cs
@@ -8,5 +8,14 @@ namespace PokeManager
 {
     class Class1
     {
+        public Class1()
+        {
+
+        }
+
+        public void save_settings(string save_name, string[] settings)
+        {
+
+        }
     }
 }
